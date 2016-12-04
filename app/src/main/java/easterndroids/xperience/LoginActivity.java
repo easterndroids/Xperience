2b0a91424049a84420e0db03694e59f23d07f316
Harsha Vardhan <ghvk12@gmail.com>
2016-12-04 12:35:43 -0400

    Registration Bug Fixed

--- app/src/main/java/easterndroids/xperience/LoginActivity.java
+++ app/src/main/java/easterndroids/xperience/LoginActivity.java
@@ -5,24 +5,37 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
-    EditText UsernameEt,PasswordEt;
+import static easterndroids.xperience.R.id.Register;
 
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText UsernameEt,PasswordEt;
+    private Button buttonRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         UsernameEt = (EditText)findViewById(R.id.UName);
         PasswordEt = (EditText)findViewById(R.id.Password);
-
+        buttonRegister = (Button) findViewById(R.id.Register);
+        buttonRegister.setOnClickListener(this);
     }
 
     public void NavigateToRegistration(View view)
     {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
+    }
+
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void NavigateToUseXP(View view)
